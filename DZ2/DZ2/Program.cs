aeb8b7a05b6ba0273cd74aac6fbd0e71d232ce5d
Bd0ptimus <43087816+Bd0ptimus@users.noreply.github.com>
2020-10-12 18:59:36 +0300

    DZ2 Fixed
    Fixed incorrect result when x != 3
    Fixed y(x)=-0.00
    Fixed result in each graph conversion point. Example : y(5)(-)=2; y(5)(+)=3

--- DZ2/DZ2/Program.cs
+++ DZ2/DZ2/Program.cs
@@ -1,6 +1,5 @@
 ﻿//Дз 2  -  Буй Тхе Зунг  -  УТС22  -  Вариант 2
 using System;
-using System.Diagnostics.CodeAnalysis;
 using m = System.Math;
 
 namespace DZ2
@@ -8,7 +7,7 @@ namespace DZ2
     class Program
     {
         //y = 0.6 * x + 1.8
-        static double segment1 (double x)
+        static double segment1(double x)
         {
             double y = 0.6 * x + 1.8;
             return y;
@@ -17,7 +16,7 @@ namespace DZ2
         //x^2 + y^2 = R^2                                                          
         static double segment2(double x, double r)
         {
-            double y = m.Sqrt(m.Pow(r,2) - x * x);
+            double y = m.Sqrt(m.Pow(r, 2) - x * x);
             return -y;
         }
 
@@ -27,69 +26,187 @@ namespace DZ2
             double y = x - 3;
             return y;
         }
-        
+
         //Funstion show result on screen
         // 2 situation:
         //             1. R>=3
         //             2. R<3
         static void show(double a, double b, double c, double r, bool checkValid)
         {
-            for (double x = -10; x <= 8.2; x += 0.2)
+            for (double x = -10.00; x <= 8.20; x += 0.20)
             {
+                x = m.Round(x, 2);
                 // When R>3 or R==3  - checkValid=false 
-                if (checkValid==false)
+                if (checkValid == false)
                 {
                     if (x <= -8)
                     {
-                        Console.WriteLine("y({0:0.00}) = {1:0.00}", x, -3);
+                        if (x == -8)
+                        {
+                            Console.WriteLine("y({0:0.00}(-)) = {1:0.00}", x, -3);
+                        }
+                        else
+                        {
+                            Console.WriteLine("y({0:0.00}) = {1:0.00}", x, -3);
+                        }
                     }
-                    else if (x > -8 && x < a)
+                    if (x >= -8 && x <= a)
                     {
-                        Console.WriteLine("y({0:0.00}) = {1:0.00}", x, segment1(x));
+                        if (x == -8)
+                        {
+                            Console.WriteLine("y({0:0.00}(+)) = {1:0.00}", x, segment1(x));
+                        }
+                        else if (x == a)
+                        {
+                            Console.WriteLine("y({0:0.00}(-)) = {1:0.00}", x, segment1(x));
+                        }
+                        else
+                        {
+                            Console.WriteLine("y({0:0.00}) = {1:0.00}", x, segment1(x));
+                        }
                     }
-                    else if (x >= a && x <= b)
+                    if (x > a && x < b)
                     {
                         Console.WriteLine("y({0:0.00}) = {1:0.00}", x, segment2(x, r));
                     }
-                    else if (x > b && x < c)
+
+                    if (x == a || x == b)
                     {
-                        Console.WriteLine("y({0:0.00}) = {1:0.00}", x, segment3(x));
+                        if (x == a)
+                        {
+                            Console.WriteLine("y({0:0.00}(+)) = {1:0.00}", x, 0);
+                        }
+                        else
+                        {
+                            Console.WriteLine("y({0:0.00}(-)) = {1:0.00}", x, 0);
+                        }
                     }
-                    else if (x >= c && x <= 8.2)
+                    if (x >= b && x <= c)
                     {
-                        Console.WriteLine("y({0:0.00}) = {1:0.00}", x, 3);
+                        if (x == b)
+                        {
+                            Console.WriteLine("y({0:0.00}(+)) = {1:0.00}", x, segment3(x));
+                        }
+                        else if (x == c)
+                        {
+                            Console.WriteLine("y({0:0.00}(-)) = {1:0.00}", x, segment3(x));
+                        }
+                        else
+                        {
+                            Console.WriteLine("y({0:0.00}) = {1:0.00}", x, segment3(x));
+                        }
+                    }
+                    if (x >= c && x <= 8)
+                    {
+                        if (x == c)
+                        {
+                            Console.WriteLine("y({0:0.00}(+)) = {1:0.00}", x, 3);
+                        }
+                        else
+                        {
+                            Console.WriteLine("y({0:0.00}) = {1:0.00}", x, 3);
+                        }
                     }
                 }
                 //When R<3 - checkValid=true
-                else if(checkValid == true)
+                else if (checkValid == true)
                 {
                     if (x <= -8)
                     {
-                        Console.WriteLine("y({0:0.00}) = {1:0.00}", x, -3);
+                        if (x == -8)
+                        {
+                            Console.WriteLine("y({0:0.00}(-)) = {1:0.00}", x, -3);
+                        }
+                        else
+                        {
+                            Console.WriteLine("y({0:0.00}) = {1:0.00}", x, -3);
+                        }
                     }
-                    else if (x > -8 && x < -3)
+                    if (x >= -8 && x <= -3.00)
                     {
-                        Console.WriteLine("y({0:0.00}) = {1:0.00}", x, segment1(x));
+                        if (x == -3)
+                        {
+                            Console.WriteLine("y({0:0.00}(-)) = {1:0.00}", x, 0);
+                        }
+                        else if (x == -8)
+                        {
+                            Console.WriteLine("y({0:0.00}(+)) = {1:0.00}", x, segment1(x));
+                        }
+                        else
+                        {
+                            Console.WriteLine("y({0:0.00}) = {1:0.00}", x, segment1(x));
+                        }
                     }
-                    else if (x > -3 && x < -r)
+                    if (x >= -3 && x <= -r)
                     {
-                        Console.WriteLine("y({0:0.00}) = Unknown!(Because R < 3)", x);
+                        if (x == -3)
+                        {
+                            Console.WriteLine("y({0:0.00}(+)) = Unknown!(Because R < 3)", x);
+                        }
+                        else if (x == -r)
+                        {
+                            Console.WriteLine("y({0:0.00}(-)) = Unknown!(Because R < 3)", x);
+                        }
+                        else
+                        {
+                            Console.WriteLine("y({0:0.00}) = Unknown!(Because R < 3)", x);
+                        }
                     }
-                    else if (x > r && x < 3)
-                    {
-                        Console.WriteLine("y({0:0.00}) = Unknown!(Because R < 3)", x);
-                    }
-                    else if ((x >= -r) && (x <= r))
+                    if (x > -r && x < r)
                     {
                         Console.WriteLine("y({0:0.00}) = {1:0.00}", x, segment2(x, r));
                     }
-                    else if (x > 3 && x < 5)
+                    if ((x == r) || (x == -r))
                     {
-                        Console.WriteLine("y({0:0.00}) = {1:0.00}", x, segment3(x));
+                        if (x == -r)
+                        {
+                            Console.WriteLine("y({0:0.00}(+)) = {1:0.00}", x, 0);
+                        }
+                        else
+                        {
+                            Console.WriteLine("y({0:0.00}(-)) = {1:0.00}", x, 0);
+                        }
                     }
-                    else if (x >= 5 && x <= 8.2)
+                    if (x >= r && x <= 3)
                     {
-                        Console.WriteLine("y({0:0.00}) = {1:0.00}", x, 3);
+                        if (x == r)
+                        {
+                            Console.WriteLine("y({0:0.00}(+)) = Unknown!(Because R < 3)", x);
+                        }
+                        else if (x == 3)
+                        {
+                            Console.WriteLine("y({0:0.00}(-)) = Unknown!(Because R < 3)", x);
+                        }
+                        else
+                        {
+                            Console.WriteLine("y({0:0.00}) = Unknown!(Because R < 3)", x);
+                        }
+                    }
+                    if (x >= 3 && x <= 5)
+                    {
+                        if (x == 3)
+                        {
+                            Console.WriteLine("y({0:0.00}(+)) = {1:0.00}", x, 0);
+                        }
+                        else if (x == 5)
+                        {
+                            Console.WriteLine("y({0:0.00}(-)) = {1:0.00}", x, segment3(x));
+                        }
+                        else
+                        {
+                            Console.WriteLine("y({0:0.00}) = {1:0.00}", x, segment3(x));
+                        }
+                    }
+                    if (x >= 5 && x <= 8)
+                    {
+                        if (x == 5)
+                        {
+                            Console.WriteLine("y({0:0.00}(+)) = {1:0.00}", x, 3);
+                        }
+                        else
+                        {
+                            Console.WriteLine("y({0:0.00}) = {1:0.00}", x, 3);
+                        }
                     }
                 }
             }
@@ -98,7 +215,7 @@ namespace DZ2
         static void Main(string[] args)
         {
             Console.WriteLine("Bui The Zung - UTS22 - Question 2");
-            double r , a =0, b=0, c=0;
+            double r, a = 0, b = 0, c = 0;
             bool checkValid = false; //checkValid=false When R>=3 ;   checkValid=true  When R<3
 
         //In Question 2 - the range of figure from -10 -> 8 and the center of circumference is(0;0)
@@ -132,7 +249,7 @@ namespace DZ2
             //When 5 <= R <= 8 : The firgure will be :
             //  -10 -> -8   ||   -8 -> a   ||    a -> b    ||  b -> c  ||  c->8
             //     y=-3     || y=0.6*x+1.8 ||  x^2+y^2=R^2 ||    y=3   ||  y=3
-            if (r >= 5 && r <= 8) 
+            if (r >= 5 && r <= 8)
             {
                 a = -r;
                 b = r;
@@ -142,7 +259,7 @@ namespace DZ2
             //When 3 < R < 5 : The firgure will be :
             //  -10 -> -8   ||   -8 -> a   ||    a -> b    ||  b -> c  ||  c->8
             //     y=-3     || y=0.6*x+1.8 ||  x^2+y^2=R^2 ||   y=x-3  ||  y=3
-            else if ((r > 3 && r < 5)||(r==3))
+            else if ((r > 3 && r < 5) || (r == 3))
             {
                 a = -r;
                 b = r;
@@ -159,9 +276,8 @@ namespace DZ2
                 c = 5;
                 checkValid = true;
             }
-            show(a,b,c,r,checkValid);
+            show(a, b, c, r, checkValid);
             Console.ReadKey();
         }
     }
 }
- 
